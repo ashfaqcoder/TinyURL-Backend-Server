aeb30f2c6715d1120cb51987e7341f8257a4f411
Ashfaq Khan <ak48573abc@gmail.com>
2026-04-13 02:10:23 -0700

    adding cache

--- Controllers/RedirectURL.js
+++ Controllers/RedirectURL.js
@@ -1,21 +1,19 @@
 import { URLs } from "../Models/urls.js";
+import { getCache, setCache } from "../Utils/redis.js";
 
 export const RedirectURL = async (req, res) => {
   const { shortId } = req.params;
   try {
-    const element = await URLs.findOne({ shortId });
-    if (!element) {
-      return res.status(404).json({
-        ok: false,
-        message: "Short URL not found",
-      });
+    const longURLfromCache = await getCache(shortId);
+    if (longURLfromCache) {
+      res.redirect(longURLfromCache);
+      return;
     }
-
-    return res.redirect(element.longUrl);
+    const resUrls = await URLs.find({ shortId: shortId });
+    const element = resUrls[0];
+    await setCache(shortId, element.longUrl);
+    res.redirect(element.longUrl);
   } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      ok: false,
-    });
+    res.status(500).json({ ok: false });
   }
 };
